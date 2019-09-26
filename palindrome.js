d89a305f6ec9171bc5f73cddfc5f2a6478db0f75
Nick Oliver <nick.oliver@rubydev.safehold.local>
2019-09-26 12:38:38 -0500

    end of chapter 7

--- palindrome.js
+++ palindrome.js
@@ -1,15 +1,19 @@
-// reverses a string.
-function reverse(string) {
-  return Array.from(string).reverse().join("");
+// Adds `reverse` to all stings.
+String.prototype.reverse = function reverse() {
+  return Array.from(this).reverse().join("");
 }
 
 // Defines a Phrase Object
 function Phrase(content) {
   this.content = content;
-  
+
+  // Returns content processed for palindrome testing.
+  this.processedContent = function processedContent() {
+    return this.content.toLowerCase();
+  }
+
   // Returns True for a palindrome, false otherwise.
   this.palindrome = function palindrome() {
-    let processedContent = this.content.toLowerCase();
-    return processedContent === reverse(processedContent);
+    return this.processedContent() === this.processedContent().reverse();
   }
 }
